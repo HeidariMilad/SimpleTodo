66ffebabbcec9e535144a5631d4dc3c887615ada
Milad <milad.heidari@me.com>
2023-05-27 12:39:51 +0330

    initials

---
+++ SimpleToDo/DBLayer.swift
@@ -0,0 +1,68 @@
+//
+//  DBLayer.swift
+//  SimpleToDo
+//
+//  Created by Milad on 5/27/23.
+//
+
+import Foundation
+import RealmSwift
+
+class TodoObject: Object {
+    @Persisted var title: String = ""
+    @Persisted var descriptionNote: String = ""
+    @Persisted var isCompleted: Bool = false
+    @Persisted var index = 0
+}
+
+class DBHelper {
+    
+    public static func RealmThread(_ block: @escaping ()->Void) {
+        DispatchQueue(label: "RealmSwift").async(execute: block)
+    }
+    
+    public static func write(object: Object) {
+        RealmThread {
+            let realm = try! Realm()
+            try! realm.write {
+                realm.add(object)
+            }
+        }
+    }
+    
+    public static func read(_ completion: @escaping ([TodoObject])->Void) {
+        Utilities.UI {
+            let realm = try! Realm()
+            let objects = realm.objects(TodoObject.self)
+            completion(Array(objects))
+        }
+    }
+    
+    public static func delete(object: Object) {
+        RealmThread {
+            let realm = try! Realm()
+            try! realm.write {
+                realm.delete(object)
+            }
+        }
+    }
+    
+    public static func realmBlock(_ block: @escaping ()->Void) {
+        Utilities.UI {
+            let realm = try! Realm()
+            try! realm.write {
+                block()
+            }
+        }
+    }
+}
+
+class Utilities {
+    public static func BG(_ block: @escaping ()->Void) {
+        DispatchQueue.global(qos: .default).async(execute: block)
+    }
+    
+    public static func UI(_ block: @escaping ()->Void) {
+        DispatchQueue.main.async(execute: block)
+    }
+}
--- SimpleToDo/ViewController.swift
+++ SimpleToDo/ViewController.swift
@@ -9,11 +9,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var items: [TodoObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        // Set the estimatedRowHeight to a non-zero number
+        self.tableView.estimatedRowHeight = 44.0
+        
+        // Use automatic dimension for rowHeight
+        self.tableView.rowHeight = UITableView.automaticDimension
+        
+//        addToList(title: "test1", description: "You can modify properties of a Realm object inside of a write transaction in the same way that you would update any other Swift or Objective-C object.")
+//        addToList(title: "test1", description: "desc1")
+//        addToList(title: "test1", description: "desc1")
+        fetchAll()
     }
 
+    func addToList(title: String, description: String) {
+        let obj = TodoObject()
+        obj.title = title
+        obj.descriptionNote = description
+
+        DBHelper.write(object: obj)
+    }
+    
+    func fetchAll(){
+        DBHelper.read { results in
+            self.items = results
+            // goto main thread
+            Utilities.UI {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func removeItem(item: TodoObject) {
+        DBHelper.delete(object: item)
+    }
 
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell") as! TodoCell
+        let item = items[indexPath.row]
+        cell.title.text = item.title
+        cell.descriptionNote.text = item.descriptionNote
+        cell.isCompleted = item.isCompleted
+        
+        DBHelper.realmBlock {
+            item.index = indexPath.row
+        }
+        
+        cell.descriptionNote?.numberOfLines = 0
+        return cell
+    }
+    
+    
+}
+
+class TodoCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var descriptionNote: UILabel!
+    var isCompleted: Bool = false
+    
+}
